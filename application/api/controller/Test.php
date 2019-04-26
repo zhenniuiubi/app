6a4ac6ea0bdeec86f49235194dd78b021e53d2b0
hehexia <minus1s@qq.com>
2019-04-26 19:02:16 +0800

    test删除多余函数

--- application/api/controller/Test.php
+++ application/api/controller/Test.php
@@ -42,49 +42,4 @@ class Test extends Common
         return show(1, 'ok', input('post.'), 201);
         // return show(1, 'ok', (new Aes())->encrypt(json_encode(input('post.'))), 201);
     }
-
-    /**
-     * 显示指定的资源
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function read($id)
-    {
-        //
-    }
-
-    /**
-     * 显示编辑资源表单页.
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * 保存更新的资源
-     *
-     * @param  \think\Request  $request
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * 删除指定资源
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function delete($id)
-    {
-        //
-    }
 }
